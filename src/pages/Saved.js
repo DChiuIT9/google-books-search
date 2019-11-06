a62b40424225b3ed68589b354dbc873627b95ce6
Daniel Chiu <cycdaniel@gmail.com>
2019-11-06 00:28:38 -0600

    Styled search and saved pages

---
+++ src/pages/Saved.js
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import DeleteBtn from "../components/DeleteBtn";
+import Jumbotron from "../components/Jumbotron";
+import API from "../utils/API";
+import { Link } from "react-router-dom";
+import { Col, Row, Container } from "../components/Grid";
+import { List, ListItem } from "../components/List";
+import { Input, TextArea, FormBtn } from "../components/Form";
+
+class Books extends Component {
+  state = {
+    books: [],
+    title: "",
+    author: "",
+    synopsis: ""
+  };
+
+  componentDidMount() {
+    this.loadBooks("Mockingbird");
+  }
+
+  loadBooks = () => {
+    API.getBooks()
+      .then(res =>
+        this.setState({ books: res.data, title: "", author: "", synopsis: "" })
+      )
+      .catch(err => console.log(err));
+  };
+
+  deleteBook = id => {
+    API.deleteBook(id)
+      .then(res => this.loadBooks())
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    if (this.state.title && this.state.author) {
+      API.saveBook({
+        title: this.state.title,
+        author: this.state.author,
+        synopsis: this.state.synopsis
+      })
+        .then(res => this.loadBooks())
+        .catch(err => console.log(err));
+    }
+  };
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="lg-12 md-12">
+            <div style={{marginTop: 30}}></div>
+            <Jumbotron>
+              <h2>(React) Google Books Search</h2>
+              <h5>Search for and Save Books of Interest</h5>
+            </Jumbotron>            
+          </Col>
+          <Col size="lg-12 md-12 sm-12">
+            <div style={{backgroundColor: "#e9ecef", padding: 20}}>
+              <p>Saved Books</p>
+            {this.state.books.length ? (
+              <List>
+                {this.state.books.map(book => (
+                  <ListItem key={book._id}>
+                    <Link to={"/books/" + book._id}>
+                      <strong>
+                        {book.title} by {book.author}
+                      </strong>
+                    </Link>
+                    <DeleteBtn onClick={() => this.deleteBook(book._id)} />
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Books;
